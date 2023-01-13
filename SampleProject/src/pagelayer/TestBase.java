3aac1e2796cbe1271913bee0213adb7b12b01191
Dattamh12 <dattamhetre007@gmail.com>
2023-01-13 21:19:24 +0530

    basic commit
    hi all..
    i have created a project on git-hub go and clone it.

---
+++ SampleProject/src/pagelayer/TestBase.java
@@ -0,0 +1,29 @@
+package pagelayer;
+
+public class TestBase {
+
+	
+		public static void main(String[] args) {
+
+
+					
+				int year = 2010;
+				int year1 = 2020;
+				int year2 = 2310;
+				int year3 = 2040;
+				int year4 = 2050;
+				
+					
+				if(((year % 4==0) && (year % 100!=0)) || (year % 100 ==0));
+					
+				System.out.println("given year is leap");
+				
+				System.out.println(year2);
+					
+				System.out.println("given year4 is leap");
+			
+				System.out.println("given year is not leap");
+		}
+
+	}
+
